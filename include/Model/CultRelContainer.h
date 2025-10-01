7715092c88275d7e57690fdc3ae70b0040efe34d
Arzyel <emeric_rivard@hotmail.com>
2025-09-30 21:26:45 -0400

    Add skeleton class CultRelContainer

---
+++ include/Model/CultRelContainer.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+#ifndef CULT_REL_CONTAINER_H
+#define CULT_REL_CONTAINER_H
+
+
+
+class CultRelContainer {
+public:
+    CultRelContainer() = default;
+    ~CultRelContainer() = default;
+
+protected:
+
+private:
+
+};
+
+#endif // CULT_REL_CONTAINER_H
