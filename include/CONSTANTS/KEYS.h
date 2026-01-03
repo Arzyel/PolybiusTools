4a8dd9d65a6df31d04db7261428f234c2106fe1d
Arzyel <emeric_rivard@hotmail.com>
2026-01-03 18:47:08 -0500

    Cleanup start

--- include/CONSTANTS/KEYS.h
+++ include/CONSTANTS/KEYS.h
@@ -49,7 +49,6 @@ constexpr const char* NATIVE_SIZE_CHAR = "native_size";
 constexpr const char* NATIVE_FEROCITY_CHAR = "native_ferocity";
 constexpr const char* NATIVE_HOSTILENESS_CHAR = "native_hostileness";
 
-//inside chat there is a convo with effects mapped to the nation effects , stopped at province effect in the modpage
 constexpr uint32_t OWNER_HASH = hash_const(OWNER_CHAR);
 constexpr uint32_t CONTROLLER_HASH = hash_const(CONTROLLER_CHAR);
 constexpr uint32_t ADD_CORE_HASH = hash_const(ADD_CORE_CHAR);
@@ -70,15 +69,6 @@ constexpr uint32_t DURATION_HASH = hash_const(DURATION_CHAR);
 constexpr uint32_t FORT_15TH_HASH = hash_const(FORT_15TH_CHAR);
 constexpr uint32_t ADD_PROVINCE_TRIGGERED_MODIFIER_HASH = hash_const(ADD_PROVINCE_TRIGGERED_MODIFIER_CHAR);
 constexpr uint32_t EXTRA_COST_HASH = hash_const(EXTRA_COST_CHAR);
-
-// makes compilation crash
-//std::unordered_map<uint32_t, const char*> locationKeys = {
-//	{OWNER_HASH,OWNER_CHAR},
-//	{CONTROLLER_HASH,CONTROLLER_CHAR}
-//};
-
-
-
 
 constexpr std::array<const char*, 4> GAMES = {
 	"Europa Universalis IV",
