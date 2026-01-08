e36ebef730329d25a028947cb7236116dc1095b8
Arzyel <emeric_rivard@hotmail.com>
2026-01-07 23:46:14 -0500

    Update metadata in executable

--- src/main.cpp
+++ src/main.cpp
@@ -14,6 +14,13 @@ int main(int argc, char* argv[]) {
     _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 
     QApplication app(argc, argv);
+    QApplication::setApplicationName("PolybiusTools");
+    QApplication::setApplicationVersion("0.1.0");
+    QApplication::setOrganizationName("Arzyel");
+    QApplication::setOrganizationDomain("https://github.com/Arzyel/PolybiusTools");
+    app.setWindowIcon(QIcon("GFX/icon/PolybiusToolsLogo.png"));
+
+
     app.setStyle(new InstantMenuStyle(QStyleFactory::create("")));
     
     FilePathHandler* filePathHandler = nullptr;
